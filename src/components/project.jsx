7a9c0464c874a20cc1a9f1d9c4b7039b83a32001
Anil Kumar <142671046+aniljasel@users.noreply.github.com>
2025-11-07 16:33:03 +0530

    new Project

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import project1 from '../assests/media/schedule-manager.png';
-import project2 from '../assests/media/paying-guest.png';
-import project3 from '../assests/media/DPDigitalAgency.png';
-import project4 from '../assests/media/Personal-Desktop.png';
-import project5 from '../assests/media/Music-Player.png';
-import project6 from '../assests/media/Snapzy.png';
-import project7 from '../assests/media/load-project.png';
+import project2 from '../assests/media/Personal-Desktop.png';
+import project3 from '../assests/media/cyberfication-3d-site.png';
+import project4 from '../assests/media/paying-guest.png';
+import project5 from '../assests/media/DPDigitalAgency.png';
+import project6 from '../assests/media/Music-Player.png';
+import project7 from '../assests/media/Snapzy.png';
+import project8 from '../assests/media/load-project.png';
 
 const Project = () => {
     const [filter, setFilter] = useState('all');
@@ -23,80 +24,80 @@ const Project = () => {
         },
         {
             id: 2,
-            title: 'Paying Guest',
-            description: 'This Hostel Management System allows you to easily manage room allocations, tenant details, and update or delete user data in real time.',
-            tags: ['HTML5', 'CSS', 'React', 'JavaScript', 'Node', 'MongoDB'],
-            image: project2,
-            demoLink: 'https://paying-guest-smoky.vercel.app',
-            codeLink: 'https://github.com/AU-Teams/paying-guest',
-            category: ['fullstack', 'backend']
-        },
-        {
-            id: 3,
-            title: 'DPDigitalAgency',
-            description: 'Developed a responsive website for DP Creatives showcasing their digital marketing, SEO, and web development services with a focus on enhancing brand presence and client engagement.',
-            tags: ['HTML5', 'CSS', 'JavaScript'],
-            image: project3,
-            demoLink: 'https://aniljasel.github.io/DPDigitalAgency',
-            codeLink: 'https://github.com/aniljasel/DPDigitalAgency',
-            category: ['frontend']
-        },
-        {
-            id: 4,
             title: 'Personal Desktop',
             description: 'This project is a unique and interactive portfolio website designed to look and function like a desktop operating system. It showcases my technical skills and projects within a creative and engaging user interface.',
             tags: ['HTML5', 'CSS', 'React', 'JavaScript', 'API', 'Tailwind CSS'],
-            image: project4,
+            image: project2,
             demoLink: 'https://personal-desktop.vercel.app/',
             codeLink: 'https://github.com/aniljasel/Personal-Desktop',
             category: ['frontend']
         },
         {
+            id: 3,
+            title: 'Cyberfication 3D Site',
+            description: 'This is an interactive website that showcases a unique 3D scroll animation experience built with modern web technologies. Here are the key features:',
+            tags: ['HTML5', 'CSS', 'JavaScript', 'Three.js', 'Canvas'],
+            image: project3,
+            demoLink: 'https://aniljasel.github.io/cyberfication-3d-site/',
+            codeLink: 'https://github.com/aniljasel/cyberfication-3d-site',
+            category: ['3D Website', 'frontend']
+        },
+        {
+            id: 4,
+            title: 'Paying Guest',
+            description: 'This Hostel Management System allows you to easily manage room allocations, tenant details, and update or delete user data in real time.',
+            tags: ['HTML5', 'CSS', 'React', 'JavaScript', 'Node', 'MongoDB'],
+            image: project4,
+            demoLink: 'https://paying-guest-smoky.vercel.app',
+            codeLink: 'https://github.com/AU-Teams/paying-guest',
+            category: ['fullstack', 'backend']
+        },
+        {
             id: 5,
+            title: 'DPDigitalAgency',
+            description: 'Developed a responsive website for DP Creatives showcasing their digital marketing, SEO, and web development services with a focus on enhancing brand presence and client engagement.',
+            tags: ['HTML5', 'CSS', 'JavaScript'],
+            image: project5,
+            demoLink: 'https://aniljasel.github.io/DPDigitalAgency',
+            codeLink: 'https://github.com/aniljasel/DPDigitalAgency',
+            category: ['frontend']
+        },
+        {
+            id: 6,
             title: 'Music Player',
             description: 'Developed a responsive web-based music player that allows users to listen to their favorite songs, play playlist songs, and control playback with an intuitive interface. Incorporated features such as play, pause, skip, and volume control to enhance user experience.',
             tags: ['HTML5', 'CSS', 'JavaScript', 'API'],
-            image: project5,
+            image: project6,
             demoLink: 'https://aniljasel.github.io/Music_Player/',
             codeLink: 'https://github.com/aniljasel/Music_Player',
             category: ['frontend']
         },
         {
-            id: 6,
+            id: 7,
             title: 'Snapzy',
             description: 'Snapzy is a modern, neon-themed YouTube-style video streaming web application built with a focus on UI aesthetics, responsiveness, and real-time video playback. The platform replicates the core features of YouTube including video search, playback controls.',
             tags: ['HTML5', 'CSS', 'JavaScript', 'API'],
-            image: project6,
+            image: project7,
             demoLink: 'https://aniljasel.github.io/Snapzy',
             codeLink: 'https://github.com/aniljasel/Snapzy',
             category: ['frontend']
         },
         {
-            id: 7,
-            title: <div className="skeleton short"></div>,
-            description: <div className="skeleton long"></div>,
+            id: 8,
+            title: 'Upcoming Project',
+             description: <><div className="skeleton long"></div><br /><div className="skeleton long"></div><br /><div className="skeleton long"></div><br /><div className="skeleton short"></div></>,
             tags: <div className="skeleton long"></div>,
-            image: project7,
+            image: project8,
             demoLink: '#',
             codeLink: '#',
             category: []
         },
         {
-            id: 7,
-            title: <div className="skeleton short"></div>,
-            description: <div className="skeleton long"></div>,
+            id: 9,
+            title: 'Upcoming Project',
+            description: <><div className="skeleton long"></div><br /><div className="skeleton long"></div><br /><div className="skeleton long"></div><br /><div className="skeleton short"></div></>,
             tags: <div className="skeleton long"></div>,
-            image: project7,
-            demoLink: '#',
-            codeLink: '#',
-            category: []
-        },
-        {
-            id: 7,
-            title: <div className="skeleton short"></div>,
-            description: <div className="skeleton long"></div>,
-            tags: <div className="skeleton long"></div>,
-            image: project7,
+            image: project8,
             demoLink: '#',
             codeLink: '#',
             category: []
